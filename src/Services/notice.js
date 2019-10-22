581fb539eefb35bcbd0964cbcd3f3ff4f8774db8
Gayeon <kgy1559@gmail.com>
2019-10-22 19:09:13 +0900

    Add function requesting notices to server

--- src/Services/notice.js
+++ src/Services/notice.js
@@ -8,3 +8,7 @@ const api = axios.create({
 export function addNewNotice(notice) {
     return axios.post(config.BACK_SERVER_PREFIX + "/notices", notice, headerConfig)
 }
+
+export function getNotices(offset, limit) {
+    return axios.get(config.BACK_SERVER_PREFIX + "/notices/" + offset + "/" + limit, "", headerConfig)
+}
