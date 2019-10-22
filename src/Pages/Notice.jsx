261583590bba50befcca36dcc124157e6fb44384
Gayeon <kgy1559@gmail.com>
2019-10-22 19:07:32 +0900

    Add request function for getting notices
    Implement function that requests notices stored at database.
    If notices are exist, append notices to list or let users know there is no notice

--- src/Pages/Notice.jsx
+++ src/Pages/Notice.jsx
@@ -5,21 +5,23 @@ import '../Css/common.css'
 import '../Css/notice.css'
 import Pagination from "./Common/Pagination";
 import * as userValidation from './Common/UserValidation'
+import * as noticeService from '../Services/notice'
 
 class Notice extends Component {
     constructor (props) {
         super(props)
         this.state = {
-            loggedInUser : userValidation.getUserLoginState(this)
+            loggedInUser : userValidation.getUserLoginState(this),
         }
     }
 
     render() {
+        // this.getNotices(this.state.offset, this.state.limit);
         return (
             <div className={"fullWidth"}>
                 <Header/>
                     <NoticeHeader/>
-                    <NoticeContents userRoll={this.state.loggedInUser.userRoll}/>
+                    <NoticeContents userRoll={this.state.loggedInUser.userRoll} offset={this.props.offset} limit={this.props.limit}/>
                 <Footer/>
             </div>
         )
@@ -41,6 +43,33 @@ class NoticeContents extends Component {
         window.location.href = "/notice/new"
     }
 
+    componentDidMount(): void {
+        this.getNotices(this.props.offset, this.props.limit)
+    }
+
+    getNotices = (offset, limit) => {
+        noticeService.getNotices(offset, limit)
+            .then(response => {
+                let notices = response.data
+                if (notices == null) {
+                    this.printNoNotices()
+                    return
+                }
+            })
+            .catch(error => {
+                //Todo : Below is temporary. If you get error, you have to confirm error to user. (console.log)
+                this.printNoNotices()
+            })
+    }
+
+    printNoNotices = () => {
+        document.getElementById("noticeTableBody").innerHTML
+        = "<tr>" +
+            "<td colspan='4'>등록된 공지가 없습니다</td>" +
+          "</tr>"
+
+    }
+
     render() {
         return (
             <div className={"contents noticeContentsDiv"}>
@@ -58,19 +87,19 @@ class NoticeContents extends Component {
                             <th className={"noticeDate"}>등록일</th>
                         </tr>
                         </thead>
-                        <tbody>
-                            <tr>
-                                <th className={"noticeNo"}>1</th>
-                                <td className={"noticeTitle"}>첫 공지입니다.</td>
-                                <td className={"noticeAuthor"}>김가연</td>
-                                <td className={"noticeDate"}>2019.09.29</td>
-                            </tr>
-                            <tr>
-                                <th className={"noticeNo"}>2</th>
-                                <td className={"noticeTitle"}>콜라톤 관련 공지입니다.</td>
-                                <td className={"noticeAuthor"}>김가연</td>
-                                <td className={"noticeDate"}>2019.08.29</td>
-                            </tr>
+                        <tbody id={"noticeTableBody"}>
+                            {/*<tr>*/}
+                            {/*    <th className={"noticeNo"}>1</th>*/}
+                            {/*    <td className={"noticeTitle"}>첫 공지입니다.</td>*/}
+                            {/*    <td className={"noticeAuthor"}>김가연</td>*/}
+                            {/*    <td className={"noticeDate"}>2019.09.29</td>*/}
+                            {/*</tr>*/}
+                            {/*<tr>*/}
+                            {/*    <th className={"noticeNo"}>2</th>*/}
+                            {/*    <td className={"noticeTitle"}>콜라톤 관련 공지입니다.</td>*/}
+                            {/*    <td className={"noticeAuthor"}>김가연</td>*/}
+                            {/*    <td className={"noticeDate"}>2019.08.29</td>*/}
+                            {/*</tr>*/}
                         </tbody>
                     </table>
                     <Pagination/>
