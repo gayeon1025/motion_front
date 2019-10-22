a892e942eb207f1dba0d8711672ab60acfda5567
Gayeon <kgy1559@gmail.com>
2019-10-22 19:37:56 +0900

    Regist new post
    To regist new post, create button on the top of table.
    Members only can write posts. For this, this page has to get user's roll.

--- src/Pages/Board.jsx
+++ src/Pages/Board.jsx
@@ -4,14 +4,23 @@ import Footer from "./Common/Footer"
 import '../Css/common.css'
 import '../Css/board.css'
 import Pagination from "./Common/Pagination";
+import * as boardService from "../Services/board";
+import * as userValidation from "./Common/UserValidation";
 
 class Board extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            loggedInUser : userValidation.getUserLoginState(this),
+        }
+    }
+
     render() {
         return (
             <div className={"fullWidth"}>
                 <Header/>
                     <BoardHeader/>
-                    <BoardContents/>
+                    <BoardContents userRoll={this.state.loggedInUser.userRoll} offset={this.props.match.params.offset} limit={this.props.match.params.limit}/>
                 <Footer/>
             </div>
         )
@@ -28,12 +37,46 @@ const BoardHeader = () => (
 )
 
 class BoardContents extends Component {
+    addNewPost = () => {
+        window.location.href = "/board/new"
+    }
+
+    componentDidMount(): void {
+        this.getPosts(this.props.offset, this.props.limit)
+    }
+
+    getPosts = (offset, limit) => {
+        boardService.getPosts(offset, limit)
+            .then(response => {
+                let posts = response.data
+                if (posts == null) {
+                    this.printNoPost()
+                    return
+                }
+            })
+            .catch(error => {
+                //Todo : Below is temporary. If you get error, you have to confirm error to user. (console.log)
+                this.printNoPost()
+            })
+
+    }
+
+    printNoPost = () => {
+        document.getElementById("boardTableBody").innerHTML
+            = "<tr>" +
+                "<td colspan='4'>등록된 공지가 없습니다</td>" +
+              "</tr>"
+
+    }
+
     render() {
         return (
             <div className={"contents boardContentsDiv"}>
                 <div className={"boardContentTitle"}>게시판</div>
                 <div className={"boardContentComment notoSansFont"}>건의사항, 교육문의 등 자유롭게 질문해 주세요 :-)</div>
-
+                <div className={"newPostButtonDiv fullWidth"} style={{display : this.props.userRoll == "ADMIN" || this.props.userRoll == "USER" ? "block" : "none"}}>
+                    <button className={"newPostButton notoSansFont"} type={"button"} onClick={ this.addNewPost }>글쓰기</button>
+                </div>
                 <div className={"boardTableDiv"}>
                     <table className={"table"}>
                         <thead>
@@ -44,7 +87,7 @@ class BoardContents extends Component {
                             <th className={"noticeDate"}>등록일</th>
                         </tr>
                         </thead>
-                        <tbody>
+                        <tbody id={"boardTableBody"}>
                         <tr>
                             <th className={"noticeNo"}>1</th>
                             <td className={"noticeTitle"}>콜라톤관련 문의드립니다.</td>
